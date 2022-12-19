02e10552d4f4377d73e6b97b0cc74353e6b52374
itnalin <itnalin@yahoo.com>
2022-12-19 22:03:18 +0530

    Adding Artist Service

---
+++ Chinook/Services/ArtistService.cs
@@ -0,0 +1,29 @@
+﻿using Chinook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chinook.Services
+{
+    public class ArtistService: IArtistService
+    {
+        private readonly IDbContextFactory<ChinookContext> DbFactory;
+        public ArtistService(IDbContextFactory<ChinookContext> dbFactory)
+        {
+            DbFactory = dbFactory;
+        }
+        public async Task<List<Artist>> GetArtists()
+        {
+            var dbContext = await DbFactory.CreateDbContextAsync();            
+            return dbContext.Artists.ToList();
+        }
+        public async Task<List<Artist>> GetArtists(string name)
+        {
+            var dbContext = await DbFactory.CreateDbContextAsync();
+            return dbContext.Artists.Where(a => a.Name.Contains(name)).ToList();
+        }
+        public async Task<List<Album>> GetAlbumsForArtist(int artistId)
+        {
+            var dbContext = await DbFactory.CreateDbContextAsync();
+            return dbContext.Albums.Where(a => a.ArtistId == artistId).ToList();
+        }
+    }
+}
---
+++ Chinook/Services/IArtistService.cs
@@ -0,0 +1,11 @@
+﻿using Chinook.Models;
+
+namespace Chinook.Services
+{
+    public interface IArtistService
+    {
+        Task<List<Artist>> GetArtists();
+        Task<List<Artist>> GetArtists(string name);
+        Task<List<Album>> GetAlbumsForArtist(int artistId);
+    }
+}
